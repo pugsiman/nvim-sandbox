75f63eebe0095b6c1a00bd2f5ed96008b6b19ca5
Aviv Levinsky <levinskyaviv@gmail.com>
2023-12-31 19:16:00 +0200

    indent

--- init.lua
+++ init.lua
@@ -271,9 +271,9 @@ require('lazy').setup({
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end
   },
 
